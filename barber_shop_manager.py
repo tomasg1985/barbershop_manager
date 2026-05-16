00507abb33c4ab2dcf54e82c03df6885260a4cfa
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-05-16 14:44:29 -0300

    i=Implementación de registro dinámico de la seccion de turnos

--- barber_shop_manager.py
+++ barber_shop_manager.py
@@ -1,10 +1,22 @@
+clientes_registrados = []
+servicios_solicitados = []
+caja_dia = []
+
 while True:
     print("1. Registrar Turno - 2. Ver Reporte del Día - 3. Cerrar Sistema")
     opcion = input("Ingrese la opcion que desea consulta: ")
     
     match opcion:
         case "1":
-            print("Funcionalidad de registro en desarrollo...")
+            
+            nombre_cliente = input("Ingrese el nombre del cliente: ").strip().title()
+            servicios = input("Seleccione el servicio que desea (Elija Corte o Barba): ").strip().title()
+            
+            clientes_registrados.append(nombre_cliente)
+            servicios_solicitados.append(servicios)
+            
+            print(f"{nombre_cliente} Tu turno para {servicios} quedó registrado con éxito")
+            
         case "2":
             print("Funcionalidad de reporte en desarrollo...")
         case "3":
