ad1d90211a1c97d10b178dab22c0889f680c6588
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-05-16 14:29:18 -0300

    Maquetción del  esqueleto  menú principal y enrutamiento con match/case

--- barber_shop_manager.py
+++ barber_shop_manager.py
@@ -2,14 +2,12 @@ while True:
     print("1. Registrar Turno - 2. Ver Reporte del Día - 3. Cerrar Sistema")
     opcion = input("Ingrese la opcion que desea consulta: ")
     
-    opcion_valida = int(opcion)
-    
-    match opcion_valida:
-        case 1:
+    match opcion:
+        case "1":
             print("Funcionalidad de registro en desarrollo...")
-        case 2:
+        case "2":
             print("Funcionalidad de reporte en desarrollo...")
-        case 3:
+        case "3":
             print("Saliendo del sistema...")
             break
         case _:
