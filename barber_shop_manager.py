b62b95271c5e93e279fcdd62d4a32b690ba8896f
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-05-16 19:02:49 -0300

    Completado del  sistema de registro de turnos y reporte de auditoría

--- barber_shop_manager.py
+++ barber_shop_manager.py
@@ -1,3 +1,5 @@
+corte = 3000
+barba = 2000
 clientes_registrados = []
 servicios_solicitados = []
 caja_dia = []
@@ -12,13 +14,36 @@ while True:
             nombre_cliente = input("Ingrese el nombre del cliente: ").strip().title()
             servicios = input("Seleccione el servicio que desea (Elija Corte o Barba): ").strip().title()
             
+            if servicios == "Corte":
+                
+                print(f"El precio por corte es de: {corte}")
+                precio_final = corte
+                
+            elif servicios == "Barba":
+                
+                print(f"El precio por barba es de: {barba}")
+                precio_final = barba
+                
+            else:
+                print(f"ERROR: Servicio no reconocido. Turno cancelado.")
+                continue
+            
+            
             clientes_registrados.append(nombre_cliente)
             servicios_solicitados.append(servicios)
+            caja_dia.append(precio_final)
             
             print(f"{nombre_cliente} Tu turno para {servicios} quedó registrado con éxito")
             
         case "2":
-            print("Funcionalidad de reporte en desarrollo...")
+            
+            if clientes_registrados:
+                print(f"\nClientes de hoy: {clientes_registrados}")
+                print(f"Servicios realizados: {servicios_solicitados}")
+                print(f"Total recaudado en caja: ${sum(caja_dia)}")
+            else:
+                print("No se registraron turnos en la jornada de hoy.")
+                
         case "3":
             print("Saliendo del sistema...")
             break
